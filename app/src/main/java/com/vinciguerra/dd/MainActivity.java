49a2573a9611a6a31ff2bd254672365296ade9da
antoinevinciguerra <antoinevinciguerra@gmail.com>
2022-05-18 14:30:55 +0200

    Version accueil land

--- app/src/main/java/com/vinciguerra/dd/MainActivity.java
+++ app/src/main/java/com/vinciguerra/dd/MainActivity.java
@@ -2,12 +2,18 @@ package com.vinciguerra.dd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowInsets;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
