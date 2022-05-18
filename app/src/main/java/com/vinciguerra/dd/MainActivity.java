ff594e3387c9a469f05ada3a12c808eaa920d046
antoinevinciguerra <antoinevinciguerra@gmail.com>
2022-05-18 14:49:11 +0200

    Version avec icon

--- app/src/main/java/com/vinciguerra/dd/MainActivity.java
+++ app/src/main/java/com/vinciguerra/dd/MainActivity.java
@@ -5,16 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowInsets;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView dragon = (ImageView) findViewById(R.id.imageViewDragon);
+        dragon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(), "CHRCHR", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    public void guidedCreation  (){}
 }
